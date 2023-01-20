b5284167bb8ec05534904010d5cce49c069cad0d
Belguith Omar <omar.belghith96@gmail.com>
2023-01-20 16:02:38 +0000

    images and initial set up of index page

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! 
-    </h1>
+    <div className="bg-gray-100 h-screen">
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <div className="w-1/2 bg-white rounded shadow p-6 m-10">
+            <h1 className="text-2xl border-b pb-3 mb-3">Alpaca Image Generator</h1>
+          </div>
+          <div className="bg-white rounded shadow p-6 m-10"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
