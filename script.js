3f05f3a225f15b27ba3b9cef1b0cca3b16ceb03e
Belguith Omar <omar.belghith96@gmail.com>
2023-01-22 21:57:11 +0000

    adding timeout for puppeteer

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ import puppeteer from 'puppeteer';
   });
   const page = await browser.newPage();
   await page.goto(process.argv[2]);
+  // we need to wait for the page to load
+  await page.waitFor(1000);
   await page.screenshot({path: process.argv[3] + '/project-screenshot.png'});
   await browser.close();
 })();
